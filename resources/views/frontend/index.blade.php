042ba052902b3644e5966dbe13916b050de87aef
MD Al-Amin <alamindev27@gmail.com>
2026-03-14 20:17:08 +0600

    Watch now button added and redesign

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -1,6 +1,43 @@
 @extends('frontend.layouts.app')
 @section('head')
     <link rel="stylesheet" href="{{ asset('frontend/css/index.css') }}">
+    <style>
+        .watch-btn {
+            background: #ffffff;
+            color: #000;
+            border: 2px solid #000;
+            padding: 12px 28px;
+            font-size: 18px;
+            font-weight: 600;
+            border-radius: 40px;
+            display: inline-flex;
+            align-items: center;
+            gap: 10px;
+            transition: 0.3s;
+        }
+
+        .watch-btn .icon {
+            background: #000;
+            color: #fff;
+            width: 35px;
+            height: 35px;
+            border-radius: 50%;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            font-size: 10px;
+        }
+
+        .watch-btn:hover {
+            background: #000;
+            color: #fff;
+        }
+
+        .watch-btn:hover .icon {
+            background: #fff;
+            color: #000;
+        }
+    </style>
 @endsection
 @section('content')
     <div class="container">
@@ -17,9 +54,6 @@
                 <div class="bengali-text">{{ $intro->heading_1 }}</div>
                 <div class="amount-text">{{ $intro->heading_2 }}</div>
             </div>
-
-
-
 
 
             <div class="col-12 col-lg-6 mx-auto">
@@ -56,157 +90,161 @@
 
             <div class="col-12 text-center mt-3 mx-auto">
                 <div class="timer-box" id="timerContainer" data-id="{{ setting()->updated_at }}">
-                    <div class="time-unit"><span class="green-num" id="hours">{{ setting()->timer['hours'] }}</span> Hrs</div>
+                    <div class="time-unit"><span class="green-num" id="hours">{{ setting()->timer['hours'] }}</span> Hrs
+                    </div>
                     <div class="timer-divider">|</div>
-                    <div class="time-unit"><span class="purple-num" id="minutes">{{ setting()->timer['minutes'] }}</span> Min</div>
+                    <div class="time-unit"><span class="purple-num" id="minutes">{{ setting()->timer['minutes'] }}</span>
+                        Min</div>
                     <div class="timer-divider">|</div>
-                    <div class="time-unit"><span class="yellow-num" id="seconds">{{ setting()->timer['seconds'] }}</span> Sec</div>
+                    <div class="time-unit"><span class="yellow-num" id="seconds">{{ setting()->timer['seconds'] }}</span>
+                        Sec</div>
                 </div>
             </div>
+        </div>
 
+        <div class="col-12 text-center mt-3 mx-auto">
+            <p class="text-white mb-1">একাউন্ট খোলার Tutorials দেখুন</p>
 
-            <div class="col-12 text-center mt-3 mx-auto">
-
-                <button class="claim-btn" onclick="return window.location.href='{{ route('frontend.videos') }}'">
-                    ▶️ একাউন্ট খোলার Tutorials দেখুন
-                </button>
-
-            </div>
-
-
-            <div class="col-12 text-center mt-3 mx-auto">
-
-                <div class="proof-card p-2">
-                    @forelse ($promos as $item)
-                        <div class="result-row d-flex justify-content-between gap-3 align-items-center px-3">
-                            <span class="day-text d-flex justify-content-start gap-3 align-items-center">
-                                <a href="{{ $item->link }}" target="_blank" title="{{ $item->name }}">
-                                    <img src="{{ asset($item->icon) }}" alt="" class="img-fluid rounded-circle"
-                                        style="vertical-align: middle; box-shadow:0px 0px 15px 0px #3498db" width="40"
-                                        height="40">
-                                </a>
-                                <span class="day-text fw-bold" style="font-size: 16px;">Promo Code - </span>
-                            </span>
-                            <span class="win-text ">
-                                <span class="d-flex justify-content-end gap-3 align-items-center">
-                                    <span style="color:#ffd700; font-size:22px;">{{ $item->promo_code }}</span>
-                                    <span class="copy-wrapper" onclick="copyToClipboard('{{ $item->promo_code }}', this)">
-                                        <span class="copy-tooltip">Copied!</span>
-                                        <span class="copy-btn" style="cursor: pointer;">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25"
-                                                fill="currentColor" class="bi bi-clipboard" viewBox="0 0 16 16">
-                                                <path
-                                                    d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1z" />
-                                                <path
-                                                    d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0z" />
-                                            </svg>
-                                        </span>
-                                    </span>
-                                </span>
-                            </span>
-                        </div>
-                    @empty
-                        <div class="col-12 mt-4 mx-auto">
-                            <div class="alert alert-info mb-0 text-dark">No promo codes available at the moment.</div>
-                        </div>
-                    @endforelse
-                </div>
-
-            </div>
-
-
-
-            <div class="col-12 text-center mt-3 mx-auto">
-
-                <div class="entry-count">
-                    Only 50 Free Entries Today! <span class="pink-text">(28 Left)</span>
-                </div>
-
-                <div class="step-container">
-                    <div class="step-box">
-                        <div class="step-title step-1-color">Step 1</div>
-                        <div class="step-icon">📇</div>
-                        <div class="step-desc"><span class="yellow-highlight">1</span> Sign Up</div>
-                    </div>
-                    <div class="step-box">
-                        <div class="step-title step-2-color">Step 2</div>
-                        <div class="step-icon">💼</div>
-                        <div class="step-desc"><span class="yellow-highlight">2</span> Deposit</div>
-                    </div>
-                    <div class="step-box">
-                        <div class="step-title step-3-color">Step 3</div>
-                        <div class="step-icon">⚽</div>
-                        <div class="step-desc">Get Free Multi</div>
-                    </div>
-                </div>
-
-            </div>
-
-            <div class="col-12 text-center mt-3 mx-auto">
-
-                <div class="proof-card p-2">
-                    <div class="proof-title">Proof Your Bankroll in <span class="yellow-highlight">3 Easy</span>
-                    </div>
-
-                    @foreach ($proofs as $item)
-                        <div class="result-row">
-                            <span class="day-text">🕒 {{ $item->time }}:</span>
-                            @if ($item->status == 'WIN')
-                                <span class="win-text">WIN <span class="status-icon win-icon">✔</span>
-                                @elseif ($item->status == 'LOSS' || $item->status == 'DRAW')
-                                    <span class="loss-text">LOSS <span class="status-icon loss-icon">✖</span>
-                            @endif
-                            </span>
-                        </div>
-                    @endforeach
-                </div>
-
-            </div>
-
-            <div class="col-12 text-center mt-3 mx-auto">
-                <div class="proof-card p-2">
-                    <div class="proof-title">Our Member and <span class="yellow-highlight">Winning Amount</span> </div>
-                    <div class="stats-container">
-                        <div class="stat-row">
-                            <div class="d-flex justify-content-start align-items-center">
-                                <span class="stat-value yellow-text counter"
-                                    data-target="{{ setting()->total_members }}">0</span>
-                                <span class="stat-value yellow-text">+</span>
-                            </div>
-                            <span class="divider-line">|</span>
-                            <span class="stat-label">Total Members</span>
-                        </div>
-                        <div class="stat-row">
-                            <div class="d-flex justify-content-start align-items-center">
-                                <span class="stat-value green-text">Tk. </span><span class="stat-value green-text counter"
-                                    data-target="{{ setting()->total_won }}">0</span><span
-                                    class="stat-value green-text">+</span>
-                            </div>
-                            <span class="divider-line">|</span>
-                            <span class="stat-label">Total Won</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-12 text-center mt-3 mx-auto">
-
-                <div class="badge-container">
-                    <div class="badge-icon-circle">#1</div>
-                    <div class="badge-text">
-                        <div class="badge-text-top">Bangladesh #1 Free Multi</div>
-                        <div class="badge-text-bottom">Paid Tips 💸 🔥</div>
-                    </div>
-                </div>
-
-            </div>
-            <div class="col-12 text-center mt-3 mx-auto">
-
-                <button class="final-claim-btn">
-                    <span>🔥</span> Claim Now Free
+            {{-- <button style="background: transparent;box-shadow: 0px 0px 20px 0px #fff;" class="border-0" onclick="return window.location.href='{{ route('frontend.videos') }}'">
+                    <img src="{{asset('frontend/img/watch-now-btn.jpg')}}" alt="" class="img-fluid" width="120" style="">
+                </button> --}}
+            <div class="text-center">
+                <button class="watch-btn" onclick="return window.location.href='{{ route('frontend.videos') }}'">
+                    <span class="icon">▶</span>
+                    Watch Now
                 </button>
             </div>
         </div>
+
+
+        <div class="col-12 text-center mt-3 mx-auto">
+
+            <div class="proof-card p-2">
+                @forelse ($promos as $item)
+                    <div class="result-row d-flex justify-content-between gap-3 align-items-center px-3">
+                        <span class="day-text d-flex justify-content-start gap-3 align-items-center">
+                            <a href="{{ $item->link }}" target="_blank" title="{{ $item->name }}">
+                                <img src="{{ asset($item->icon) }}" alt="" class="img-fluid rounded-circle"
+                                    style="vertical-align: middle; box-shadow:0px 0px 15px 0px #3498db" width="40"
+                                    height="40">
+                            </a>
+                            <span class="day-text fw-bold" style="font-size: 16px;">Promo Code - </span>
+                        </span>
+                        <span class="win-text ">
+                            <span class="d-flex justify-content-end gap-3 align-items-center">
+                                <span style="color:#ffd700; font-size:22px;">{{ $item->promo_code }}</span>
+                                <span class="copy-wrapper" onclick="copyToClipboard('{{ $item->promo_code }}', this)">
+                                    <span class="copy-tooltip">Copied!</span>
+                                    <span class="copy-btn" style="cursor: pointer;">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25"
+                                            fill="currentColor" class="bi bi-clipboard" viewBox="0 0 16 16">
+                                            <path
+                                                d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1z" />
+                                            <path
+                                                d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0z" />
+                                        </svg>
+                                    </span>
+                                </span>
+                            </span>
+                        </span>
+                    </div>
+                @empty
+                    <div class="col-12 mt-4 mx-auto">
+                        <div class="alert alert-info mb-0 text-dark">No promo codes available at the moment.</div>
+                    </div>
+                @endforelse
+            </div>
+
+        </div>
+
+
+
+        <div class="col-12 text-center mt-3 mx-auto">
+
+            <div class="entry-count">
+                Only 50 Free Entries Today! <span class="pink-text">(28 Left)</span>
+            </div>
+
+            <div class="step-container">
+                <div class="step-box">
+                    <div class="step-title step-1-color">Step 1</div>
+                    <div class="step-icon">📇</div>
+                    <div class="step-desc"><span class="yellow-highlight">1</span> Sign Up</div>
+                </div>
+                <div class="step-box">
+                    <div class="step-title step-2-color">Step 2</div>
+                    <div class="step-icon">💼</div>
+                    <div class="step-desc"><span class="yellow-highlight">2</span> Deposit</div>
+                </div>
+                <div class="step-box">
+                    <div class="step-title step-3-color">Step 3</div>
+                    <div class="step-icon">⚽</div>
+                    <div class="step-desc">Get Free Multi</div>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="col-12 text-center mt-3 mx-auto">
+
+            <div class="proof-card p-2">
+                <div class="proof-title">Proof Your Bankroll in <span class="yellow-highlight">3 Easy</span>
+                </div>
+
+                @foreach ($proofs as $item)
+                    <div class="result-row">
+                        <span class="day-text">🕒 {{ $item->time }}:</span>
+                        @if ($item->status == 'WIN')
+                            <span class="win-text">WIN <span class="status-icon win-icon">✔</span>
+                            @elseif ($item->status == 'LOSS' || $item->status == 'DRAW')
+                                <span class="loss-text">LOSS <span class="status-icon loss-icon">✖</span>
+                        @endif
+                        </span>
+                    </div>
+                @endforeach
+            </div>
+
+        </div>
+
+        <div class="col-12 text-center mt-3 mx-auto">
+            <div class="proof-card p-2">
+                <div class="proof-title">Our Member and <span class="yellow-highlight">Winning Amount</span> </div>
+                <div class="stats-container">
+                    <div class="stat-row">
+                        <div class="d-flex justify-content-start align-items-center">
+                            <span class="stat-value yellow-text counter"
+                                data-target="{{ setting()->total_members }}">0</span>
+                            <span class="stat-value yellow-text">+</span>
+                        </div>
+                        <span class="divider-line">|</span>
+                        <span class="stat-label">Total Members</span>
+                    </div>
+                    <div class="stat-row">
+                        <div class="d-flex justify-content-start align-items-center">
+                            <span class="stat-value green-text">Tk. </span><span class="stat-value green-text counter"
+                                data-target="{{ setting()->total_won }}">0</span><span
+                                class="stat-value green-text">+</span>
+                        </div>
+                        <span class="divider-line">|</span>
+                        <span class="stat-label">Total Won</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-12 text-center mt-3 mx-auto">
+
+            <div class="badge-container">
+                <div class="badge-icon-circle">#1</div>
+                <div class="badge-text">
+                    <div class="badge-text-top">Bangladesh #1 Free Multi</div>
+                    <div class="badge-text-bottom">Paid Tips 💸 🔥</div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
     </div>
 
 
