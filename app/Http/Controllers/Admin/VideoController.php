21aed59d043c128d75dc3a51a386d076f321b590
MD Al-Amin <alamindev27@gmail.com>
2026-03-29 16:31:36 +0600

    Update video Controller

--- app/Http/Controllers/Admin/VideoController.php
+++ app/Http/Controllers/Admin/VideoController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
+use App\Http\Controllers\Controller;
 use App\Http\Requests\VideoRequest;
 use App\Models\Video;
 use Illuminate\Http\Request;
